79d8cddd48c3b3acb8b80f661301187335c90b68
Sekomane <130669303+Sekomane@users.noreply.github.com>
2023-07-21 23:28:01 +0200

    Add files via upload

---
+++ infor.js
@@ -0,0 +1,10 @@
+function prices() {
+  var first=document.getElementById("Lemon");
+  var final=document.getElementById("calc");
+
+  var Lemon=first.value;
+  var total=final.value;
+
+  total=Lemon*25-10;
+
+}
